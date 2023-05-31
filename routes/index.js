ecf2e79858ae4c8020996f1aac0fe035b348b186
ELI NURHAYATI <99855451+elinurhayati@users.noreply.github.com>
2023-05-31 12:35:24 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,45 @@
 import express from "express";
-import { 
-    getUsers,  
+import {  
     RegistUsers,  
     LoginUsers,  
-    LogoutUsers, 
     UpdateUsers 
 } from "../controllers/users.js";
 
 import { 
-    getFarms,
+    getAllFarms,
     createFarms, 
     UpdateFarms,
     deleteFarms,
     LoginFarms
 } from "../controllers/farms.js";
 
-import { tokenRefresh } from "../controllers/refreshTokenForUsers.js";
+import { 
+    getAllBookmarks,
+    addBookmarks,
+    getFarmDetails,
+    deleteBookmarks
+} from "../controllers/bookmarks.js";
+
 import { verifyTokenForUsers } from "../middleware/verifyTokenForUsers.js";
 
 const router = express.Router();
 
 // Endpoints for User
-router.get('/users', verifyTokenForUsers, getUsers);
 router.post('/regist-users', RegistUsers);
 router.post('/login-users', LoginUsers);
-router.put('/update-users/:id_user', UpdateUsers);
-router.get('/token-users', tokenRefresh);
-router.delete('/logout-users', LogoutUsers);
+router.put('/update-users/:id_user', verifyTokenForUsers, UpdateUsers);
 
 // Endpoints for Farms
-router.get('/farms', getFarms);
-router.post('/users/:id_user/createFarms', createFarms);
-router.post('/login-farms/:id_user', LoginFarms);
-router.put('/farms/:id_user/updateFarms/:id_farm', UpdateFarms);
-router.delete('/farms/:id_user/deleteFarms/:id_farm', deleteFarms);
+router.get('/farms', verifyTokenForUsers, getAllFarms);
+router.post('/farms/:id_user/createFarms', verifyTokenForUsers, createFarms);
+router.post('/login-farms/:id_user', verifyTokenForUsers, LoginFarms);
+router.put('/farms/updateFarms/:id_farm', verifyTokenForUsers, UpdateFarms);
+router.delete('/farms/:id_user/deleteFarms/:id_farm', verifyTokenForUsers, deleteFarms);
+
+// Endpoints for bookmark
+router.get('/bookmarks/:id_user', verifyTokenForUsers, getAllBookmarks);
+router.post('/bookmarks/:id_user/:id_farm', verifyTokenForUsers, addBookmarks);
+router.get('/detail-bookmarks/:id_farm', verifyTokenForUsers, getFarmDetails);
+router.delete('/delete-bookmarks/:id_bookmark', verifyTokenForUsers, deleteBookmarks);
 
 export default router;
