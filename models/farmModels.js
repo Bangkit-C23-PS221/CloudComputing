940cb11d19b79931594095f692f8439b5d44d256
ELI NURHAYATI <99855451+elinurhayati@users.noreply.github.com>
2023-05-31 12:34:26 +0700

    Update farmModels.js

--- models/farmModels.js
+++ models/farmModels.js
@@ -1,20 +1,29 @@
 import { Sequelize } from "sequelize";
 import db from "../config/database.js";
+import UserAyamHub from "./userModels.js";
 
 const { DataTypes } = Sequelize;
 
 const Farms = db.define('tb_farms', {
     id_farm: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.INTEGER(11),
         primaryKey: true,
         autoIncrement: true,
         /*field: 'id' //renamed column */
     },
     id_user: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER(11),
+        references: {
+            model: UserAyamHub,
+            key: 'id_user',
+            onDelete: 'NO ACTION',
+            onUpdate: 'CASCADE'
+        }
     },
-    username_farm: {
+    name_farm: {
         type: DataTypes.STRING,
+        unique: true,
+        required: true,
     },
     type_chicken: {
         type: DataTypes.STRING,
@@ -23,13 +32,13 @@ const Farms = db.define('tb_farms', {
         type: DataTypes.STRING,
     },
     age_chicken: {
-        type: DataTypes.STRING,
+        type: DataTypes.DATE,
     },
     weight_chicken: {
-        type: DataTypes.STRING,
+        type: DataTypes.CHAR,
     },
     stock_chicken: {
-        type: DataTypes.STRING,
+        type: DataTypes.CHAR,
     },
     desc_farm: {
         type: DataTypes.STRING,
@@ -38,13 +47,7 @@ const Farms = db.define('tb_farms', {
         type: DataTypes.STRING,
     },
     pic_farm: {
-        type: DataTypes.STRING,
-    },
-    longtitude: {
-        type: DataTypes.STRING,
-    },
-    latitude: {
-        type: DataTypes.STRING,
+        type: DataTypes.BLOB,
     },
     status: {
         type: DataTypes.STRING,
@@ -52,5 +55,7 @@ const Farms = db.define('tb_farms', {
 }, {
     freezeTableName: true
 });
+
+Farms.belongsTo(UserAyamHub, { foreignKey: 'id_user', onDelete: 'NO ACTION', onUpdate: 'CASCADE' });
 
 export default Farms;
