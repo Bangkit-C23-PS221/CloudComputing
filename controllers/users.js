14243ffd5498d9ce335ecf24a031ebbfab9baaa7
ELI NURHAYATI <99855451+elinurhayati@users.noreply.github.com>
2023-06-02 10:42:18 +0700

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,64 +1,39 @@
 import { Sequelize } from "sequelize";
 import db from "../config/database.js";
-import UserAyamHub from "./userModels.js";
 
 const { DataTypes } = Sequelize;
 
-const Farms = db.define('tb_farms', {
-    id_farm: {
+const UserAyamHub = db.define('tb_users', {
+    id_user: {
         type: DataTypes.INTEGER(11),
         primaryKey: true,
         autoIncrement: true,
         /*field: 'id' //renamed column */
     },
-    id_user: {
-        type: DataTypes.INTEGER(11),
-        references: {
-            model: UserAyamHub,
-            key: 'id_user',
-            onDelete: 'NO ACTION',
-            onUpdate: 'CASCADE'
-        }
+    name: {
+        type: DataTypes.STRING,
+        required: true,
     },
-    name_farm: {
+    password: {
+        type: DataTypes.STRING,
+        required: true,
+    },
+    email: {
         type: DataTypes.STRING,
         unique: true,
         required: true,
     },
-    type_chicken: {
+    phone: {
         type: DataTypes.STRING,
     },
-    price_chicken: {
+    userLevel: {
         type: DataTypes.STRING,
     },
-    age_chicken: {
-        type: DataTypes.DATE,
-    },
-    weight_chicken: {
-        type: DataTypes.CHAR,
-    },
-    stock_chicken: {
-        type: DataTypes.CHAR,
-    },
-    desc_farm: {
-        type: DataTypes.STRING,
-    },
-    address_farm: {
-        type: DataTypes.STRING,
-    },
-    photo: {
-        type: DataTypes.STRING,
-    },
-    photo_url: {
-        type: DataTypes.STRING,
-    },
-    status: {
-        type: DataTypes.STRING,
-    },
+    refresh_token:{
+        type: DataTypes.TEXT,
+    }
 }, {
     freezeTableName: true
 });
 
-Farms.belongsTo(UserAyamHub, { foreignKey: 'id_user', onDelete: 'NO ACTION', onUpdate: 'CASCADE' });
-
-export default Farms;
+export default UserAyamHub;
