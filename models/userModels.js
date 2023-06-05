6aeafa83d7f5eed13922bf2ec2b7c1041734fdd6
ELI NURHAYATI <99855451+elinurhayati@users.noreply.github.com>
2023-06-05 10:41:37 +0700

    Update userModels.js

--- models/userModels.js
+++ models/userModels.js
@@ -1,5 +1,5 @@
-import { Sequelize } from "sequelize";
-import db from "../config/database.js";
+const { Sequelize } = require("sequelize");
+const db = require("../config/database.js");
 
 const { DataTypes } = Sequelize;
 
@@ -36,4 +36,4 @@ const UserAyamHub = db.define('tb_users', {
     freezeTableName: true
 });
 
-export default UserAyamHub;
+module.exports = UserAyamHub;
